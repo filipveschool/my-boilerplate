07b563dbf60724b23967dddcf118806987db63bb
mchweb <michael.chugaev@gmail.com>
2016-01-21 15:37:23 +0600

    ver2

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,20 +20,28 @@ module.exports = function(grunt) {
         cssmin: {
             dist: {
                 files: {
-                    'prod/css/screen.min.css': ['css/screen.css']
+                    'prod/css/screen.min.css': ['css/tidy.css']
                 }
             }
         },
-        sass: {
+        compass: {
+            dist: {
+                options: {
+                    sassDir: 'sass',
+                    cssDir: 'css'
+                }
+            }
+        },
+        /*sass: {
             options: {
                 sourceMap: false
             },
             dist: {
                 files: {
-                    'css/screen.css': 'scss/screen.scss'
+                    'css/screen.css': 'sass/screen.scss'
                 }
             }
-        },
+        },*/
         processhtml: {
             build: {
                 files: {
@@ -45,27 +53,40 @@ module.exports = function(grunt) {
             main: {
                 files: [
                     {expand: true, src: ['favicon.ico'], dest: 'prod/', filter: 'isFile'},
-                    {expand: true, src: ['js/vendor/*'], dest: 'prod/js/vendor/', filter: 'isFile'}
+                    {expand: true, src: ['js/vendor/*'], dest: 'prod/', filter: 'isFile'}
                 ],
             },
         },
         watch: {
             options: { livereload: true, },
             css: {
-                files: ['scss/*.scss'],
-                tasks: ['sass']
+                files: ['sass/*.scss'],
+                tasks: ['compass']
+            },
+            html: {
+                files: ['*.html','**/*.css']
+            }
+        },
+        uncss: {
+            dist: {
+                files: {
+                    'css/tidy.css': ['index.html']
+                }
+            }
         }
-    }
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-sass');
+    //grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-processhtml');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-uncss');
+    grunt.loadNpmTasks('grunt-contrib-compass');
 
-    grunt.registerTask('default', ['uglify', 'imagemin', 'sass', 'cssmin', 'processhtml', 'copy']);
+    grunt.registerTask('default', ['compass', 'uglify', 'imagemin', 'cssmin', 'processhtml', 'copy']);
+    grunt.registerTask('landing', ['compass', 'uglify', 'imagemin', 'uncss', 'cssmin', 'processhtml', 'copy']);
 
 };
